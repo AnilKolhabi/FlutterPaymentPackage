16fd67795dc903ac3690a20c506c746e966644f2
Anil Sah <anil.deltatech@gmail.com>
2021-03-03 10:46:34 +0545

    Flutter Payment

---
+++ test/flutter_payment_package_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_payment_package/flutter_payment_package.dart';
+
+void main() {
+  test('adds one to input values', () {
+    final payment = Payment();
+  });
+}
